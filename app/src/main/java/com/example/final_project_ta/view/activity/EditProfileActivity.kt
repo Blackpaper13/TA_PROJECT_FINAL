07b87c50cc21e983dd17e294583dcb8cfb9b1fa3
Blackpaper13 <chris321.yusti@gmail.com>
2022-11-25 16:04:20 +0700

    tambah LAyout Editr Profile dan Intent

--- app/src/main/java/com/example/final_project_ta/view/activity/EditProfileActivity.kt
+++ app/src/main/java/com/example/final_project_ta/view/activity/EditProfileActivity.kt
@@ -2,6 +2,7 @@ package com.example.final_project_ta.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.final_project_ta.R
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
