72f1b47f6c27aa64da64203a14fa03b9ea2218fd
Blackpaper13 <chris321.yusti@gmail.com>
2022-11-25 23:23:30 +0700

    front-end sudah selesai bekerja

--- app/src/main/java/com/example/final_project_ta/view/activity/EditProfileActivity.kt
+++ app/src/main/java/com/example/final_project_ta/view/activity/EditProfileActivity.kt
@@ -1,12 +1,38 @@
 package com.example.final_project_ta.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.final_project_ta.R
+import com.example.final_project_ta.databinding.ActivityEditProfileBinding
+import com.example.final_project_ta.view.fragment.HomeFragment
+import com.example.final_project_ta.view.fragment.SettingsFragment
 
 class EditProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditProfileBinding
+    private val REQUEST_CODE_GALLERY = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSaveChangeProfil.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonChangeProfil.setOnClickListener {
+            openGalleryForImage()
+        }
     }
+
+    private fun openGalleryForImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_CODE_GALLERY)
+    }
+
 }
