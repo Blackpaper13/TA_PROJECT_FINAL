9ce26bc65c7fac2dad19bf12e1e2fdafa1d10b22
Blackpaper13 <chris321.yusti@gmail.com>
2022-11-25 14:48:14 +0700

    tambah LAyout Settings

---
+++ app/src/main/java/com/example/final_project_ta/view/activity/EditProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.final_project_ta.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
