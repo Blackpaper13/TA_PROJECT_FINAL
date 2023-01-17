2c522293f70b00190ea4d79ab1290f2e73c1a885
Blackpaper13 <chris321.yusti@gmail.com>
2023-01-17 19:51:06 +0700

    final_revisi_3

--- app/src/main/java/com/example/final_project_ta/view/fragment/ControlFragment.kt
+++ app/src/main/java/com/example/final_project_ta/view/fragment/ControlFragment.kt
@@ -6,15 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.final_project_ta.R
+import com.example.final_project_ta.databinding.FragmentControlBinding
+import com.example.final_project_ta.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ControlFragment : Fragment() {
+
+    private var _binding: FragmentControlBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database : DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control, container, false)
+        _binding = FragmentControlBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        database = Firebase.database.reference
+        auth = FirebaseAuth.getInstance()
+
+
     }
 
 
