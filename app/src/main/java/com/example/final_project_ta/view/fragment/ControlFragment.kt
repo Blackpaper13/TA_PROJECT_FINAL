84c1b4773505db2322b1f0186feec146080e3596
Blackpaper13 <chris321.yusti@gmail.com>
2023-01-20 02:56:12 +0700

    final_revisi_6_finalisasi

--- app/src/main/java/com/example/final_project_ta/view/fragment/ControlFragment.kt
+++ app/src/main/java/com/example/final_project_ta/view/fragment/ControlFragment.kt
@@ -1,5 +1,7 @@
 package com.example.final_project_ta.view.fragment
 
+import android.annotation.SuppressLint
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -35,25 +37,67 @@ class ControlFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("CommitPrefEdits")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         database = Firebase.database.reference
         auth = FirebaseAuth.getInstance()
 
-        binding.tb1.setOnCheckedChangeListener { _, isChecked ->
+
+        val my_pref = "swtich_prefs"
+        val SWITCH_STATUS = "switch_status"
+        val SWITCH_STATUS1 = "switch_status1"
+        val SWITCH_STATUS2 = "switch_status2"
+        val SWITCH_STATUS3 = "switch_status3"
+        val SWITCH_STATUS4 = "switch_status4"
+
+
+        val myPreferences = this.activity?.getSharedPreferences(my_pref, MODE_PRIVATE)
+        val myEditor = myPreferences?.edit()
+
+        var switch_status  = false
+        var switch_status1  = false
+        var switch_status2  = false
+        var switch_status3  = false
+        var switch_status4  = false
+
+        if (myPreferences != null) {
+            switch_status= myPreferences.getBoolean(SWITCH_STATUS, false)
+            switch_status1= myPreferences.getBoolean(SWITCH_STATUS1, false)
+            switch_status2= myPreferences.getBoolean(SWITCH_STATUS2, false)
+            switch_status3= myPreferences.getBoolean(SWITCH_STATUS3, false)
+            switch_status4= myPreferences.getBoolean(SWITCH_STATUS4, false)
+
+        }
+
+        binding.tb1.isChecked = switch_status
+        binding.tb2.isChecked = switch_status1
+        binding.tb3.isChecked = switch_status2
+        binding.tb4.isChecked = switch_status3
+        binding.tb5.isChecked = switch_status4
+
+
+
+        binding.tb1.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
                 val ganti_on =  mapOf("statusPintu" to on_status)
                 val userId = auth.currentUser!!.uid
                 database.child("Users").child(userId).child("ujicoba").updateChildren(ganti_on).addOnSuccessListener {
                     Toast.makeText(activity, "ON", Toast.LENGTH_SHORT).show()
                 }
+                myEditor?.putBoolean(SWITCH_STATUS, true);
+                myEditor?.apply()
+                binding.tb1.isChecked = true
             }else {
                 val ganti_off =  mapOf("statusPintu" to off_status)
                 val userId = auth.currentUser!!.uid
                 database.child("Users").child(userId).child("ujicoba").updateChildren(ganti_off).addOnSuccessListener {
                     Toast.makeText(activity, "OFF", Toast.LENGTH_SHORT).show()
                 }
+                myEditor?.putBoolean(SWITCH_STATUS, false);
+                myEditor?.apply()
+                binding.tb1.isChecked = false
             }
         }
 
@@ -63,12 +107,19 @@ class ControlFragment : Fragment() {
                 database.child("test").child("ujicoba/relay").updateChildren(ganti_on).addOnSuccessListener {
                     Toast.makeText(activity, "ON", Toast.LENGTH_SHORT).show()
                 }
+                myEditor?.putBoolean(SWITCH_STATUS1, true);
+                myEditor?.apply()
+                binding.tb2.isChecked = true
             }else {
                 val ganti_off =  mapOf("relay2" to off_status)
                 database.child("test").child("ujicoba/relay").updateChildren(ganti_off).addOnSuccessListener {
                     Toast.makeText(activity, "OFF", Toast.LENGTH_SHORT).show()
                 }
+                myEditor?.putBoolean(SWITCH_STATUS1, false);
+                myEditor?.apply()
+                binding.tb2.isChecked = false
             }
+
         }
 
         binding.tb3.setOnCheckedChangeListener { _, isChecked ->
@@ -77,11 +128,17 @@ class ControlFragment : Fragment() {
                 database.child("test").child("ujicoba/relay").updateChildren(ganti_on).addOnSuccessListener {
                     Toast.makeText(activity, "ON", Toast.LENGTH_SHORT).show()
                 }
+                myEditor?.putBoolean(SWITCH_STATUS2, true);
+                myEditor?.apply()
+                binding.tb3.isChecked = true
             }else {
                 val ganti_off =  mapOf("relay1" to off_status)
                 database.child("test").child("ujicoba/relay").updateChildren(ganti_off).addOnSuccessListener {
                     Toast.makeText(activity, "OFF", Toast.LENGTH_SHORT).show()
                 }
+                myEditor?.putBoolean(SWITCH_STATUS2, false);
+                myEditor?.apply()
+                binding.tb3.isChecked = false
             }
         }
 
@@ -91,11 +148,17 @@ class ControlFragment : Fragment() {
                 database.child("test").child("ujicoba/relay").updateChildren(ganti_on).addOnSuccessListener {
                     Toast.makeText(activity, "ON", Toast.LENGTH_SHORT).show()
                 }
+                myEditor?.putBoolean(SWITCH_STATUS3, true);
+                myEditor?.apply()
+                binding.tb4.isChecked = true
             }else {
                 val ganti_off =  mapOf("relay3" to off_status)
                 database.child("test").child("ujicoba/relay").updateChildren(ganti_off).addOnSuccessListener {
                     Toast.makeText(activity, "OFF", Toast.LENGTH_SHORT).show()
                 }
+                myEditor?.putBoolean(SWITCH_STATUS3, false);
+                myEditor?.apply()
+                binding.tb4.isChecked = false
             }
         }
 
@@ -105,11 +168,17 @@ class ControlFragment : Fragment() {
                 database.child("test").child("ujicoba/relay").updateChildren(ganti_on).addOnSuccessListener {
                     Toast.makeText(activity, "ON", Toast.LENGTH_SHORT).show()
                 }
+                myEditor?.putBoolean(SWITCH_STATUS4, true);
+                myEditor?.apply()
+                binding.tb5.isChecked = true
             }else {
                 val ganti_off =  mapOf("relay4" to off_status)
                 database.child("test").child("ujicoba/relay").updateChildren(ganti_off).addOnSuccessListener {
                     Toast.makeText(activity, "OFF", Toast.LENGTH_SHORT).show()
                 }
+                myEditor?.putBoolean(SWITCH_STATUS4, false);
+                myEditor?.apply()
+                binding.tb5.isChecked = false
             }
         }
 
